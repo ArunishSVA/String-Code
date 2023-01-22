4eceaebf4e78552099468aac3825fed78d4b9f28
ArunishSVA <arunishshiva.apr12@gmail.com>
2023-01-22 19:59:02 +0530

    inheritanace

---
+++ src/org/sts/test/CandidateDetails.java
@@ -0,0 +1,16 @@
+package org.sts.test;
+
+public class CandidateDetails extends FatherDetails {
+	public void candidateName() {
+System.out.println("My name is Arunish S V");
+
+	}
+public static void main(String[] args) {
+	
+	
+	
+	CandidateDetails cd = new CandidateDetails();
+	cd.candidateName();
+	cd.fatherName();
+}
+}
---
+++ src/org/sts/test/FatherDetails.java
@@ -0,0 +1,11 @@
+package org.sts.test;
+
+public class FatherDetails {
+	public void fatherName() {
+		System.out.println("My father Name is A Vivekanandhan");
+		
+
+	}
+	
+
+}
