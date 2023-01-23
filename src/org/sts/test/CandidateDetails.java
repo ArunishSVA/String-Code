093739b6f4d132451a7dd1cb47df3aaed8599ff9
ArunishSVA <arunishshiva.apr12@gmail.com>
2023-01-23 23:32:20 +0530

    changes added by Babu

--- src/org/sts/test/CandidateDetails.java
+++ src/org/sts/test/CandidateDetails.java
@@ -3,7 +3,7 @@ package org.sts.test;
 public class CandidateDetails extends FatherDetails {
 	public void candidateName() {
 System.out.println("My name is Arunish S V");
-
+System.out.println("Branch name is Babu");
 	}
 public static void main(String[] args) {
 	
